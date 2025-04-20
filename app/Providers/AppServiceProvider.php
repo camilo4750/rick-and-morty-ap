ade6f9ef435a862488f641d4ed0e0e767a995ccc
Camilo Alarcon <camilo4750@gmail.com>
2025-04-19 20:14:28 -0500

    feat(provider): Add registration and providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Interfaces\Repositories\CharacterRepositoryInterface;
+use App\Interfaces\Services\CharacterServiceInterface;
+use App\Repositories\Character\CharacterRepository;
+use App\Services\Character\CharacterService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CharacterServiceInterface::class, CharacterService::class);
+        $this->app->bind( CharacterRepositoryInterface::class, CharacterRepository::class);
     }
 
     /**
